d40d5005ec2aa8c4e76bd5b644c0857f63e725eb
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-05-27 06:17:17 -0500

    Return doubled integer

---
+++ js/8thKyu/doubleInteger.js
@@ -0,0 +1,27 @@
+/*
+    Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+    let doubled = i + i
+    return doubled;
+}
+
+console.log(doubleInteger(2)); //4
+console.log(doubleInteger(3)); //6
+console.log(doubleInteger(8)); //16
+
+/*
+    PREP
+    Problem: Return doubled integer
+    P: integer
+    R: return, integer
+    E: 
+        function double(int){
+            return int * int;
+        }
+    P:
+        take in the integer
+        double the integer
+        return integer
+*/

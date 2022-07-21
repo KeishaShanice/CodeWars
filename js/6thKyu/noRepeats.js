d22b4df48253648cb98b9acd808d60753a0997c5
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-07-21 10:14:13 -0500

    Return true if NaN

---
+++ js/6thKyu/noRepeats.js
@@ -0,0 +1,28 @@
+/*
+    Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+    
+    For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+    With list [20,37,20,21] and number 1, the result would be [20,37,21].
+*/
+
+/*
+    Problem: Remove duplicate if > n
+    P: array, integers
+    R: return, new list, integers
+    E:
+        function name(arr, num){
+            let newArray = []
+            let index = 0
+            let count = 0;
+            for(let i = 0; i < arr.length; i++){
+                index = arr[i]
+                for(let j = i)
+
+                }
+            }
+
+            return newArray
+        }
+    P
+*/

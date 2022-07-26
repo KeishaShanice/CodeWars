a88633d5327389034435c17041510adaa4916754
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-07-26 08:39:05 -0500

    Add to an array n times

---
+++ swift/kataTwist.swift
@@ -0,0 +1,13 @@
+/*
+    This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+    Add the value "codewars" to the array websites/Websites 1,000 times.
+*/
+
+func KataTwist() {
+    var websites: [String] = []
+
+    for i in 1...1000 {
+        websites.append("codewars")
+    }
+}
